3a6dcd42b953925397f04c5c26f4fd64dd5f7ea1
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2023-02-07 09:04:16 +0000

    If applied, this commit will create selectOperation function
    Also, updated mobile styling.

--- js/script.js
+++ js/script.js
@@ -15,25 +15,83 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-  return operator(num1, num2);
-}
-
-function populate() {
-  const display = document.getElementById("display");
-  display.innerText = displayValue;
-}
-
-function inputHandler(e) {
-  if (e.target.classList.contains("nums")) {
-    displayValue = e.target.innerText;
-    populate();
+  let computation = "";
+  if (isNaN(num1) || isNaN(num2)) return;
+  switch (operator) {
+    case "+":
+      computation = add(Number.parseInt(num1), Number.parseInt(num2));
+      break;
+    case "-":
+      computation = subtract(Number.parseInt(num1), Number.parseInt(num2));
+      break;
+    case "*":
+      computation = multiply(Number.parseInt(num1), Number.parseInt(num2));
+      break;
+    case "/":
+      computation = divide(Number.parseInt(num1), Number.parseInt(num2));
+      break;
+    default:
+      return;
   }
+  currentOperand = computation;
+  previousOperand = "";
+  operation = "";
 }
 
-let displayValue = 0;
-const btns = document.querySelectorAll(".btns");
+function selectOperation(operator) {
+  if (currentOperand === "") return;
+  operation = operator;
+  if (previousOperand !== "") {
+    operate(operator, currentOperand, previousOperand);
+  }
+  previousOperand = currentOperand;
+  currentOperand = "";
+}
 
-btns.forEach((btn) => btn.addEventListener("click", inputHandler));
+function updateDisplay(number) {
+  currentOperand = currentOperand.toString() + number.toString();
+  if (currentOperand.length > 6) {
+    currentOperand = currentOperand.slice(0, 6);
+  }
+  display.innerText = currentOperand;
+}
+
+function clearDisplay() {
+  currentOperand = "";
+  previousOperand = "";
+  operator = "";
+  display.innerText = currentOperand;
+}
+
+const display = document.getElementById("display");
+const numbers = document.querySelectorAll(".nums");
+const operators = document.querySelectorAll(".ops");
+const equal = document.getElementById("equal");
+const clear = document.getElementById("clear");
+let currentOperand = "";
+let previousOperand = "";
+let operation = "";
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    updateDisplay(number.innerText);
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    selectOperation(operator.innerText);
+  });
+});
+
+equal.addEventListener("click", () => {
+  operate(operation, currentOperand, previousOperand);
+  updateDisplay(currentOperand);
+});
+
+clear.addEventListener("click", () => {
+  clearDisplay();
+});
 
 // console.log(add(2, 2));
 // console.log(subtract(5, 2));
